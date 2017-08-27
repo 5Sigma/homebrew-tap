5007b0dd78abc33a550ea0cbaefd4f10b50206cc
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-27 00:58:16 -0400

    spyder version v0.3.0

--- spyder.rb
+++ spyder.rb
@@ -1,9 +1,9 @@
 class Spyder < Formula
   desc "API testing in the terminal."
   homepage "https://github.com/5Sigma/spyder"
-  url "https://github.com/5Sigma/spyder/releases/download/v0.2.1/spyder_0.2.1_darwin_amd64.tar.gz"
-  version "0.2.1"
-  sha256 "993b648176af35e6090a9751bb6b3e1a92c3896524a6913aa9fce0c839ae4e34"
+  url "https://github.com/5Sigma/spyder/releases/download/v0.3.0/spyder_0.3.0_darwin_amd64.tar.gz"
+  version "0.3.0"
+  sha256 "550c262d9b555effffecb090cedcbe87d4e2f5120c8a5a29de348768adfb3091"
 
   def install
     bin.install "spyder"
