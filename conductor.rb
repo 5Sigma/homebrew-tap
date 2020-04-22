bbae78d2f49e22b67ca08321665768027bcd54e9
Joe Bellus <joe@5sigma.io>
2020-04-22 00:41:53 -0400

    Added conductor placeholder

---
+++ conductor.rb
@@ -0,0 +1,12 @@
+class Spyder < Formula
+  desc "Project runner and stack manager"
+  homepage "https://github.com/5Sigma/conductor"
+  url "https://github.com/5Sigma/conductor/releases/download/v0.3.0/spyder_0.3.0_darwin_amd64.tar.gz"
+  version "0.3.0"
+  sha256 "550c262d9b555effffecb090cedcbe87d4e2f5120c8a5a29de348768adfb3091"
+
+  def install
+    bin.install "conductor"
+  end
+
+end
