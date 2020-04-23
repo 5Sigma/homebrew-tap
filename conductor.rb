fd90f0908fa101a081f7b24c8adfc45c8dce0902
Joe Bellus <joe@5sigma.io>
2020-04-22 23:03:52 -0400

    Updated conductor version to 0.1.0

--- conductor.rb
+++ conductor.rb
@@ -1,9 +1,9 @@
 class Conductor < Formula
   desc "Project runner and stack manager"
   homepage "https://github.com/5Sigma/conductor"
-  url "https://github.com/5Sigma/conductor/releases/download/v0.3.0/spyder_0.3.0_darwin_amd64.tar.gz"
-  version "0.3.0"
-  sha256 "550c262d9b555effffecb090cedcbe87d4e2f5120c8a5a29de348768adfb3091"
+  url "https://github.com/5Sigma/conductor/releases/download/v0.1.0/conductor-darwin.zip"
+  version "0.1.0"
+  sha256 "d2287dd000a43971353804da334c58b580afd497a9af676c095474cf16d53834"
 
   def install
     bin.install "conductor"
