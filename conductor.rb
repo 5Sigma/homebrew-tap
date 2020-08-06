7aeed128efa5e8edfd71ac53224a14e6dc8a1825
Joe Bellus <joe@5sigma.io>
2020-08-06 14:08:42 -0400

    Update conductor version

--- conductor.rb
+++ conductor.rb
@@ -2,7 +2,7 @@ class Conductor < Formula
   desc "Project runner and stack manager"
   homepage "https://github.com/5Sigma/conductor"
   url "https://github.com/5Sigma/conductor/releases/download/v0.3.0/conductor-darwin.zip"
-  version "0.3.0"
+  version "0.3.1"
   sha256 "088e8127b98b204ae389735abcf366e0bc2e31870eda5e3b6862938179a80eac"
 
   def install
