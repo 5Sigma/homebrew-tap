c54e9e11ba2f4e0b8953ad3d7b587a1ef057eee4
Joe Bellus <joe@5sigma.io>
2020-09-08 13:42:54 -0400

    Update conductor.rb

--- conductor.rb
+++ conductor.rb
@@ -4,6 +4,7 @@ class Conductor < Formula
   url "https://github.com/5Sigma/conductor/releases/download/v0.4.0/conductor-darwin.zip"
   version "0.4.0"
   sha256 "db6a87c012ed8461de8cd2f608a19accf523c1de9a221ebb9fa8113a59638730"
+  depends_on "openssl"
 
   def install
     bin.install "conductor"
